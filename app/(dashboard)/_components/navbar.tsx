3dd553f61a9df0732011da4a72d6c1fd9e422551
Nat Chen <chen_natalie@outlook.com>
2024-02-01 18:41:55 +0800

    feat: navbar

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,8 +1,17 @@
+"use client";
+
+import { InviteButton } from "./sidebar/invite-button";
+import { SearchInput } from "./sidebar/search-input";
+
 export const Navbar = () => {
+  const organization = true;
   return (
-    <div className="flex items-center gap-x-4 p-5 bg-green-500">
-      <div className="hidden lg-flex lg:flex-1 bg-yellow-500">Search</div>
-      <div>User Button</div>
+    <div className="flex items-center gap-x-4 p-6">
+      <div className="hidden lg:flex lg:flex-1">
+        <SearchInput />
+      </div>
+      <div className="block lg:hidden flex-1">OrganizationSwitcher</div>
+      {organization && <InviteButton />}
     </div>
   );
 };
