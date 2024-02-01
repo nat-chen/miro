cc4dca95621a341f6373215bd13047650860e87d
Nat Chen <chen_natalie@outlook.com>
2024-02-01 09:36:48 +0800

    feat: basic layout

---
+++ app/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,8 @@
+export const Navbar = () => {
+  return (
+    <div className="flex items-center gap-x-4 p-5 bg-green-500">
+      <div className="hidden lg-flex lg:flex-1 bg-yellow-500">Search</div>
+      <div>User Button</div>
+    </div>
+  );
+};
---
+++ middleware.ts
@@ -0,0 +1,5 @@
+export default () => {};
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
