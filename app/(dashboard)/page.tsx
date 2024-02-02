f579cdabeb28a4821e9dd82be4a0b55232e04725
Nat Chen <chen_natalie@outlook.com>
2024-02-02 10:01:48 +0800

    feat: empty scene

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,5 +1,24 @@
-const DashboardPage = () => {
-  return <div>Dashboard Root page</div>;
+import { EmptyOrg } from "./_components/empty-org";
+import { BoardList } from "./_components/board-list";
+
+type DashboardPageProps = {
+  searchParams: {
+    search?: string;
+    favorites?: string;
+  };
+};
+
+const DashboardPage = ({ searchParams }: DashboardPageProps) => {
+  const organization = null;
+  return (
+    <div className="flex-1 h-[calc[100%-80px] p-6">
+      {!organization ? (
+        <EmptyOrg />
+      ) : (
+        <BoardList orgId={organization.id} query={searchParams} />
+      )}
+    </div>
+  );
 };
 
 export default DashboardPage;
